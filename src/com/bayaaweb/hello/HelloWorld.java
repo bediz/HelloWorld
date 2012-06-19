11efe704c1c1e11b3fcd8daab3ad2e4fca634dac
İbrahim Bediz <ibrahim.bediz@gmail.com>
2012-06-19 20:52:27 +0300

    first

---
+++ src/com/bayaaweb/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.bayaaweb.hello;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world from bayaaWeb!!!");
+	}
+
+}
